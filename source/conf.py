6fd3365c229f8f70108a8002c2a9bafc4e3060ce
denn_moe <denn_moe@mailbox.org>
2023-03-11 15:06:28 +0100

    lieber nicht...

--- source/conf.py
+++ source/conf.py
@@ -39,6 +39,6 @@ html_title = "schule"
 
 html_theme = 'pydata_sphinx_theme'
 
-html_theme_options = {
-   "announcement": "Hamburger + Silbermann = <3",
-}
+# html_theme_options = {
+#    "announcement": "Hamburger + Silbermann = <3",
+# }
